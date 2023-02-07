d00d31ac892ca647f10775ba06739249d9d01833
jinhyuk-hong <syed56@naver.com>
2023-02-07 19:08:48 +0900

    day01

---
+++ ex01/day01/day01.java
@@ -0,0 +1,10 @@
+package java200.ex01.day01;
+
+public class day01 {
+	public static void main(String[]args) {
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("Java 200제 공부 시작");
+		System.out.println("초급부터");
+		System.out.println("+++++++++++++++++++++++++");
+	}
+}
