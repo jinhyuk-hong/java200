807c029c4f993c82d553f4ac2c9af9995fd3066f
jinhyuk-hong <syed56@naver.com>
2023-02-07 19:12:11 +0900

    day01_01

---
+++ ex01/day01/day01_1.java
@@ -0,0 +1,15 @@
+package java200.ex01.day01;
+
+public class day01_1 {
+
+	public static void main(String[] args) {
+	boolean isL=false;
+	char suit='H';
+	int year =2023;
+	double latitude = 3.3213215;
+	System.out.println(latitude+year);
+	
+
+	}
+
+}
