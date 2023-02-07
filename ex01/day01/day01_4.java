671d32651a53282601eb6d92d34e7f3a06ef9262
jinhyuk-hong <syed56@naver.com>
2023-02-07 19:31:11 +0900

    day01_03

---
+++ ex01/day01/day01_4.java
@@ -0,0 +1,17 @@
+package java200.ex01.day01;
+
+public class day01_4 {
+
+	public static void main(String[] args) {
+		int a = 123;
+		int b = 345;
+		int c = 678;
+		
+		int to = a+b+c;
+		int av = to/3;
+		System.out.printf("국어%d점, 영어%d점, 수학%d점\n",a,b,c);
+		System.out.printf("총점%d점, 평균%d점\n",to,av);
+
+	}
+
+}
