753bc5a609d73321c18bffc199c422bb72027258
jinhyuk-hong <syed56@naver.com>
2023-02-10 00:02:57 +0900

    day3_2

---
+++ ex01/day03/day03_4.java
@@ -0,0 +1,13 @@
+package java200.ex01.day03;
+
+public class day03_4 {
+	public static void main(String[] args) {
+		int a=99;
+		if(a%2==1) {
+			a=a*3+1;
+		}else {
+			a=a/2;
+		}
+		System.out.printf("°è»ê ÈÄ=%d\n",a);
+	}
+}
