b8644ef125eaca2f8e2dcdb296e2437125f4bf14
jinhyuk-hong <syed56@naver.com>
2023-02-11 22:48:21 +0900

    day4_1

---
+++ ex01/day04/day04_1.java
@@ -0,0 +1,12 @@
+package java200.ex01.day04;
+
+public class day04_1 {
+
+	public static void main(String[] args) {
+		int a = 99;
+		a=(a%2==1)? a*3+1 : a/2;
+		System.out.printf("°è»êÈÄ=%d\n",a);
+
+	}
+
+}
