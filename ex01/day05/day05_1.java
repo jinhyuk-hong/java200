f48d8ff13c31e66f923ba862f4879cb04a19b45e
jinhyuk-hong <syed56@naver.com>
2023-02-12 23:49:34 +0900

    day5

---
+++ ex01/day05/day05_1.java
@@ -0,0 +1,15 @@
+package java200.ex01.day05;
+
+public enum day05_1 {
+	PHYSICAL(23), EMOTIONAL(28), INTELLECTUAL(33);
+	private final int peiValue;
+	day05_1(int pie){
+		this.peiValue=pie;
+	}
+	public int getPei() {
+		return peiValue;
+	}
+}
+enum FRUIT{
+	APPLE,BANANA,MANGO
+}
