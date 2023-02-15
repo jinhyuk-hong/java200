2519951a6e7164ce797791f9f9cbcd7a6b5f2616
jinhyuk-hong <syed56@naver.com>
2023-02-15 23:53:10 +0900

    day7_1

---
+++ ex01/day08/day08_1.java
@@ -0,0 +1,16 @@
+package java200.ex01.day08;
+
+public class day08_1 {
+	public static  void main(String[]args) {
+		int range=10;
+		int start=1;
+		int a=0, b=0, c=0;
+		while(true) {
+			a=(int)(range*Math.random()+start);
+			b=(int)(range*Math.random()+start);
+			c=(int)(range*Math.random()+start);
+			if(a!=b&&b!=c&&c!=a)break;
+		}
+		System.out.printf("%d\t%d\t%d",a,b,c);
+	}
+}
