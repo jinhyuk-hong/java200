d82ca1ae2f245eb75797f86cf1713f5bf315274b
jinhyuk-hong <syed56@naver.com>
2023-02-17 23:41:34 +0900

    day10_1

---
+++ ex01/day10/day10_1.java
@@ -0,0 +1,13 @@
+package java200.ex01.day10;
+
+public class day10_1 {
+
+	public static void main(String[] args) {
+		int[]mm= {1,6,16,22,23,33};
+		for(int m:mm) {
+			System.out.println(m+"\t");
+		}
+		System.out.println();
+	}
+
+}
