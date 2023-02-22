7671205775996de8b84f2135f7594375acd23c01
jinhyuk-hong <syed56@naver.com>
2023-02-22 23:47:10 +0900

    day12_1

---
+++ ex01/day12/day12_1.java
@@ -0,0 +1,23 @@
+package java200.ex01.day12;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class day12_1 {
+
+	public static void main(String[] args) {
+		List<Integer>ilist=new ArrayList<>();
+		int s =0;
+		for(int i=1; i<=10; i++) {
+			ilist.add(i);
+		}
+		ilist=ilist.stream().filter(i->i%2==1).map(i -> i*i).collect(Collectors.toList());
+		ilist.forEach(i->{System.out.printf(i+"\t");});
+		System.out.println();
+		s=ilist.stream().reduce(0, Integer::sum);
+		System.out.println("1~10 사이의 홀수에 대한 제곱합:"+s);
+
+	}
+
+}
