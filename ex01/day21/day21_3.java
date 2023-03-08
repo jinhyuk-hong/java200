ecbc6ca9dfdac7b5d82747d703253910f4698077
jinhyuk-hong <syed56@naver.com>
2023-03-08 21:11:40 +0900

    day21_3'

---
+++ ex01/day21/day21_3.java
@@ -0,0 +1,33 @@
+package java200.ex01.day21;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class day21_3 {
+
+	public static void main(String[] args) {
+		ArrayList<String>htmls=new ArrayList<String>();
+		String newurls="https://ko.wikipedia.org/wiki/ISO_3166-1";
+		URL url=null;
+		try {
+		url=new URL(newurls);	
+		
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(url.openStream(),"utf-8"),8);
+		String line =null;
+		while((line = reader.readLine())!=null) {
+			if(!line.trim().equals("")) {
+				htmls.add(line.trim());
+				}
+			}
+		}catch(Exception e) {
+			System.out.println("Wiki flag Parsing error!!");
+		}
+		for(String str:htmls) {
+			System.out.println(str);
+		}
+	}
+
+}
