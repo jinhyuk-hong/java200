f44498bdda39470a4d951e1f61ec1a400b042970
jinhyuk-hong <syed56@naver.com>
2023-03-10 21:01:12 +0900

    day23_2'

--- ex01/day22/day22_2.java
+++ ex01/day22/day22_2.java
@@ -2,6 +2,9 @@ package java200.ex01.day22;
 
 import java.io.IOException;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public class day22_2 {
 	String result;
 	public void getAllHtml(String newurls) {
@@ -18,5 +21,41 @@ public class day22_2 {
 	}
 	public  void printHtml() {
 		System.out.println(result);
+	}
+	public String getTimeDate(String aurl) {
+		String newurls = "https://www.billboard.com/charts/hot-100/";
+		HttpGet request = new HttpGet(newurls);
+		CloseableHttpClient httpClient=null;
+		CloseableHttpRequest response = null;
+		Document doc = null;
+		String sdate="";
+		try {
+			httpClient = HttpClients.createDefault();
+			response = httpClient.execute(request);
+			StringBuffer sb = new StringBuffer();
+			HttpEntity entity = response.getEntity();
+			if(entity != null) {
+				String result = EntityUtills.toStirng(entity);
+				sb.append(result);
+			}
+			doc=Jsoup.parse(sb.toString());
+			String id="charts";
+			Element eid=doc.getElementById(id);
+			sdate=eid.appendChild("data-chart-date");
+		}catch(IOException e) {
+			System.out.println("Billboard Parsing error!!!");
+			
+		}
+		return sdate != null?
+				RestDay.saturday(sdate): RestDay.saturday();
+		
+	}
+	public static void main(String[]args) {
+		RequestFromBillboardHot rfw = new RequestFromBillboardHot();
+		String a = "https://www.billboard.com/charts/hot-100";
+		String rs = rfw.getTimeDate(a);
+		System.out.println("¿Ãπ¯¡÷"+rs);
+		rfw.getAllHtml(a+rs);
+		rfw.printHtml();
 	}
 }
