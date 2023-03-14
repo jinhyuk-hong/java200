485115ac51b055f6715d506992eff0edd75881d4
jinhyuk-hong <syed56@naver.com>
2023-03-14 21:03:45 +0900

    day25_1

---
+++ ex01/day24/day24_1.java
@@ -0,0 +1,28 @@
+package java200.ex01.day24;
+
+import java200.ex01.day23.day23_1;
+
+public class day24_1 {
+	public static final int SUTDA = 2;
+	public static final String[] SUIT= {"H","C"};
+	public static final String[] VALU= {"A","2","3","4","5","6","7","8","9","T"};
+	
+	public static int toVal(day23_1 c) {
+		return toVal(c.getCardVal().charAt(1));
+	}
+	public static int toVal(day23_1 c, int index) {
+		return toVal(c.getCardVal().charAt(index));
+	}
+	public static int toVal(char cc) {
+		int tot=0;
+		switch(cc) {
+		case 'A':tot=1;break;
+		case 'T':tot=10;break;
+		case 'J':tot=11;break;
+		case 'Q':tot=12;break;
+		case 'K':tot=13;break;
+		default:tot=cc-'0';break;
+		}
+		return tot;
+	}
+}
