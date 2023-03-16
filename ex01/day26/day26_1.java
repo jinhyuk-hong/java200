0c0c5ca00fda3cbb204be112d55f35519f7f3155
jinhyuk-hong <syed56@naver.com>
2023-03-16 21:55:34 +0900

    day26_1

---
+++ ex01/day26/day26_1.java
@@ -0,0 +1,50 @@
+package java200.ex01.day26;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import java200.ex01.day24.day24_1;
+import java200.ex01.day25.day25_1;
+
+public class day26_1 {
+	private List<day25_1> cards = new ArrayList<day25_1>();
+	public day26_1() {
+		cards.clear();
+	}
+	public List<day25_1>getCards(){
+		return cards;
+	}
+	public int count() {
+		return cards.size();
+		}
+	public day25_1 getCard(int index) {
+		return cards.get(index);
+	}
+	public void make() {
+		cards.clear();
+		int suit= day24_1.SUIT.length;
+		int valu= day24_1.VALU.length;
+		int count=0;
+		while(count!=valu*suit) {
+			day25_1 c = new day25_1();
+			if(!cards.contains(c)) {
+				cards.add(c);
+				count++;
+			}
+		}
+	}
+	public void shuffle() {
+		Collections.shuffle(cards);
+	}
+	public void print() {
+		int valu = day24_1.VALU.length;
+		for(int i=0; i<cards.size(); i++) {
+			day25_1 c = cards.get(i);
+			System.out.printf("%s",c.toString());
+			if((i+1)%valu==0) {
+				System.out.println();
+			}
+		}
+	}
+}
