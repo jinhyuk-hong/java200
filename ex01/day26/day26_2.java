45a10227087ac6f9575848744c095d5cf1f085cc
jinhyuk-hong <syed56@naver.com>
2023-03-16 21:59:09 +0900

    day26_2

---
+++ ex01/day26/day26_2.java
@@ -0,0 +1,18 @@
+package java200.ex01.day26;
+
+public class day26_2 {
+
+	public static void main(String[] args) {
+		day26_1 cc = new day26_1();
+		cc.make();
+		cc.print();
+		System.out.println("------------------");
+		cc.shuffle();
+		cc.print();
+		System.out.println("------------------");
+		cc.shuffle();
+		cc.print();
+
+	}
+
+}
