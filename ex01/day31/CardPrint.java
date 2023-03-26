c566cfbe123f76258df66db8f47a25ba673f4ca1
jinhyuk-hong <syed56@naver.com>
2023-03-26 22:07:34 +0900

    바카라 족보만드는 중-2

--- ex01/day31/CardPrint.java
+++ ex01/day31/CardPrint.java
@@ -21,8 +21,37 @@ public class CardPrint {
 		System.out.println("첫번쨰 카드 수:");
 		System.out.println(num(0));
 		
-		System.out.println("뱅커의 2장의 카드합:");
-		System.out.println(sumD());
-		TotalDealer = sumD();
+		System.out.println("두번째 카드");
+		System.out.println(cc.getCards().get(1));
+		System.out.println("두번쨰 카드 수:");
+		System.out.println(num(1));
+		
+		System.out.println("플레이어의 2장의 카드합 :");
+		System.out.println(sumP());
+		TotalPlayer = sumP();
+		
+		System.out.println("--------------");
+		
+		if(rule.natural(sumP(), sumD())) {
+			System.out.println("예외처리로 바로 겨룹니다!");
+			TotalPlayer = sumP();
+		}else if(rule.stand(sumP(), sumD())) {
+			standPrintP1();
+		}else if(rule.thirdCardDealer(sumP())) {
+			System.out.println("세번째 카드");
+			System.out.println("세번쨰 카드 :"+cc.getCards().get(4));
+			System.out.println("첫번쨰 카드 수:" + num(4));
+			System.out.println("----------------");
+			System.out.println("플레이어 총 카드 합:"+total(sumP(),num(4)));
+			TotalPlayer=total(sumP(),num(4));
+		}
+		System.out.println("------------");
+		System.out.println();
+		System.out.println("<뱅커 카드>");
+		System.out.println("------------");
+		System.out.println("1번째 카드:");
+		System.out.println(cc.getCards().get(2));
+		System.out.println("1번째 카드 수:");
+		System.out.println(num(2));
 	}
 }
