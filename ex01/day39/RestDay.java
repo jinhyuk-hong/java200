35110add755662220e083772a07b041d31ab06fb
jinhyuk-hong <syed56@naver.com>
2023-04-07 17:02:32 +0900

    캘린더 활용

---
+++ ex01/day39/RestDay.java
@@ -0,0 +1,30 @@
+package java200.ex01.day39;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class RestDay {
+	public static boolean isRest(Calendar tod) {
+		boolean isRest=false;
+		if(tod.get(Calendar.DAY_OF_WEEK)==Calendar.SATURDAY|| tod.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY) {
+			isRest=true;
+		}
+		return isRest;
+	}
+	public static String toYMD(Calendar dd) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		return sdf.format(dd.getTime());
+	}
+	public static void main(String[] args) {
+		Calendar cal= Calendar.getInstance();
+		int lastDay=cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		for(int i=1; i<=lastDay; i++) {
+			cal.set(Calendar.DAY_OF_MONTH,i);
+			if(isRest(cal)) {
+				System.out.println(toYMD(cal)+"isRest Day");
+			}
+		}
+
+	}
+
+}
