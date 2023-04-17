b0901d3ee81f152b3d82913bb0ee52245639a8af
jinhyuk-hong <syed56@naver.com>
2023-04-17 19:09:14 +0900

    사용자 지정 달력 만들기 - 2

--- ex01/day41/Calendar.java
+++ ex01/day41/Calendar.java
@@ -32,5 +32,42 @@ public class Calendar {
 		}
 		return tot;
 	}
-
+	public int total(int year, int month, int date) {
+		int tot=total(year,month);
+		return tot+date;
+	}
+	public int lastDay(int year, int month) {
+		int tot=0;
+		if(isLeapYear(year)) {
+			tot=LEAPY[month-1];
+		}else {
+			tot=PLAIN[month-1];
+		}
+		return tot;
+	}
+	public void showCal(int year, int month) {
+		String s1=String.format("\t\t%d년\t%d월",year,month);
+		String t1 = String.format("일\t월\t화\t수\t목\t금\t토");
+		System.out.println(s1);
+		System.out.println(t1);
+		
+		int dateOfMonth =(1+total(year, month,1)-1)%7;
+		for(int i=0; i< dateOfMonth; i++) {
+			System.out.println("\t");
+		}
+		for(int i=1; i<=lastDay(year,month); i++) {
+			System.out.printf("%d\t",i);
+			if((i+dateOfMonth)%7==0) {
+				System.out.println();
+			}
+		}
+		System.out.println("\n-----------------------");
+	}
+	public void showCal(int year) {
+		String s1 = String.format("\t\t\t%d년",year);
+		System.out.println(s1);
+		for(int i=1; i<13; i++) {
+			showCal(year, i);
+		}
+	}
 }
