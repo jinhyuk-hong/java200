2413f94062548cd0b1bf5b45f721d465ebeb552d
jinhyuk-hong <syed56@naver.com>
2023-04-19 15:23:30 +0900

    Lotto 프로그램-1

---
+++ ex01/lotto/Lotto6NumMain.java
@@ -0,0 +1,12 @@
+package java200.ex01.lotto;
+
+public class Lotto6NumMain {
+
+	public static void main(String[] args) {
+		Lotto6Num lot = new Lotto6Num(45,7);
+		lot.make();
+		lot.print();
+
+	}
+
+}
