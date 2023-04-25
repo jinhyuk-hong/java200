f46372e624ffdb4b01ebe784be7a78dd11fb3831
jinhyuk-hong <syed56@naver.com>
2023-04-25 19:47:40 +0900

    RSA 암호화하기

--- ex01/day45/RSA.java
+++ ex01/day45/RSA.java
@@ -12,7 +12,20 @@ public class RSA {
 		p=(int)(Math.random()*START);
 		q=(int)(Math.random()*START);
 		while((p==q)||(p<100||q<100)||(!Java200Math.isPrime(p)||!Java200Math.isPrime(q))) {
-			
+			p = (int)(Math.random()*START);
+			q = (int)(Math.random()*START);
 		}
+	}
+	public void makeN() {
+		n=p*q;
+	}
+	public int getN() {
+		return n;
+	}
+	public void print() {
+		System.out.printf("(p,q)=(%d,%d)\n"+"(n,phi)=(%d,%d)\n(e,d)=(%d,%d)\n",p,q,n,phiN,e,d);
+	}
+	public void makePHIN() {
+		this.phiN=(p-1)*(q-1);
 	}
 }
