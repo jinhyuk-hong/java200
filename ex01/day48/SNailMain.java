26b3257a0f99f029641b81fe01ebd7fcdbdc9f1a
jinhyuk-hong <syed56@naver.com>
2023-05-03 22:12:52 +0900

    시계방향 달팽이-2

---
+++ ex01/day48/SNailMain.java
@@ -0,0 +1,10 @@
+package java200.ex01.day48;
+
+public class SNailMain {
+	public static void main(String[] args) {
+		//시계 방향
+		SNail snail=new SNail(7, 7);
+		snail.make();
+		snail.print();
+	}
+}
